b54d8fd6aae2cd6cc02be18687d5cb6249fd0567
Matteo Bissoli <matteo.bissoli@netgrid.it>
2016-04-01 11:44:54 +0200

    correzione signature metodi rbtree

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -2,14 +2,34 @@ package it.netgrid.rbtrees;
 
 public class RbTree<T extends Comparable<String>> {
 
-	private void leftRotate(RbTreeElement<T> element) {}
-	private void rightRotate(RbTree<T> element) {}
-	private void insertFixup() {}
-	private void deleteFixup() {}
-	
-	public void insert(T element) {}
-	public void delete(T element) {}
+	private RbTreeElement<T> root;
+
+	private void leftRotate(RbTreeElement<T> element) {
+	}
+
+	private void rightRotate(RbTreeElement<T> element) {
+	}
+
+	private void insertFixup(RbTreeElement<T> element) {
+	}
+
+	private void deleteFixup(RbTreeElement<T> element) {
+	}
+
+	public void insert(T element) {
+	}
+
+	public void delete(T element) {
+	}
+
 	public T get(String element) {
 		return null;
-	}	
+	}
+
+	public RbTreeElement<T> getRoot() {
+		return null;
+	}
+
+	private void setRoot(RbTreeElement<T> root) {
+	}
 }
