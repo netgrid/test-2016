6e6358c2ebb054d5d6f200c66d710e89046f597b
Matteo Bissoli <matteo.bissoli@netgrid.it>
2016-04-01 13:03:25 +0200

    Unit tests per decoratore

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -1,5 +1,7 @@
 package it.netgrid.rbtrees;
 
+import java.util.List;
+
 public class RbTree<T extends Comparable<String>> {
 
 	private RbTreeElement<T> root;
@@ -32,5 +34,9 @@ public class RbTree<T extends Comparable<String>> {
 
 	private void setRoot(RbTreeElement<T> root) {
 		this.root = root;
+	}
+	
+	public List<T> getList(String element) {
+		return null;
 	}
 }
--- rb-trees/src/test/java/it/netgrid/rbtrees/BasicRbTreeDecoratorTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/BasicRbTreeDecoratorTest.java
@@ -1,5 +1,44 @@
 package it.netgrid.rbtrees;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import io.codearte.jfairy.Fairy;
+
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
 public class BasicRbTreeDecoratorTest {
 
+	private BasicRbTreeDecorator<Object> classUnderTest;
+	private Fairy fairy;
+	
+	@Before
+	public void init() {
+		this.classUnderTest = new BasicRbTreeDecorator<Object>(null);
+		this.fairy = Fairy.create();
+	}
+	
+	@Test
+	public void testNullCompareToNull() {
+		int result = this.classUnderTest.compareTo(null);
+		assertThat("null equals null", result, equalTo(0));
+	}
+	
+	@Test
+	public void testNullCompareToEmptyString() {
+		int result = this.classUnderTest.compareTo("");
+		assertThat("null smaller than empty", result, equalTo(-1));
+	}
+	
+	@Test
+	public void testInertNotEmptyCompareTo() {
+		String item = this.fairy.textProducer().latinSentence();
+		String compare = this.fairy.textProducer().latinSentence();
+		this.classUnderTest = new BasicRbTreeDecorator<Object>(item);
+		int result = this.classUnderTest.compareTo(compare);
+		int check = item.compareTo(compare);
+		
+		assertThat("same results", result, equalTo(check));
+	}
 }
