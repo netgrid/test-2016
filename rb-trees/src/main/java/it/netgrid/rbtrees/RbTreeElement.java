96723233d423454c5192e62170b8ce31cc0277b5
Matteo Bissoli <matteo.bissoli@netgrid.it>
2016-04-01 11:35:09 +0200

    Spike progetto

---
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTreeElement.java
@@ -0,0 +1,14 @@
+package it.netgrid.rbtrees;
+
+public interface RbTreeElement<T extends Comparable<T>> {
+
+	public enum Color{BLACK,RED}
+	
+	public boolean isBlack();
+	public boolean isRed();
+	public RbTreeElement<T> getParent();
+	public Color getColor();
+	public RbTreeElement<T> getLeft();
+	public RbTreeElement<T> getRight();
+	public T getElement();
+}
