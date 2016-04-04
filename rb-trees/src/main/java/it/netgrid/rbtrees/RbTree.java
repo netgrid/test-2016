fe9195dade05ddb169fecdf5c565c7518fbe5ccf
Giacomo Bonadiman <bonadimangiacomo@gmail.com>
2016-04-04 10:29:01 +0300

    Fixato il fixup

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -16,45 +16,73 @@ public class RbTree<T extends Comparable<String>> {
 	}
 
 	private void deleteFixup(RbTreeElement<T> element) {
-		
-		RbTreeElement<T> w; 
-		
-		while(element!=root&&element.isBlack()==true){
-			
-			if(element==element.getParent().getLeft()){
-				 w=element.getParent().getRight();
-				if(w.isRed()){
+
+		RbTreeElement<T> w;
+
+		while (element != root && element.isBlack() == true) {
+
+			if (element == element.getParent().getLeft()) {
+				w = element.getParent().getRight();
+				if (w.isRed()) {
 					w.setColor(Color.BLACK);
 					element.getParent().setColor(Color.RED);
 					leftRotate(element.getParent());
-					w=element.getParent().getRight();
+					w = element.getParent().getRight();
 				}
-				
-				if(w.getLeft().isBlack()&&w.getRight().isBlack()){
+
+				if (w.getLeft().isBlack() && w.getRight().isBlack()) {
 					w.setColor(Color.RED);
 					element = element.getParent();
 				}
-				
-				else{ if (w.getRight().isBlack()){
+
+				else {
+					if (w.getRight().isBlack()) {
 						w.getLeft().setColor(Color.BLACK);
 						w.setColor(Color.RED);
 						rightRotate(w);
 						w = element.getParent().getRight();
-						w.setColor(element.getParent().getColor());
-						element.getParent().setColor(Color.BLACK);
-				        w.getRight().setColor(Color.BLACK);
-				        leftRotate(element.getParent());
-				        element = root;
-					
+					}
+					w.setColor(element.getParent().getColor());
+					element.getParent().setColor(Color.BLACK);
+					w.getRight().setColor(Color.BLACK);
+					leftRotate(element.getParent());
+					element = root;
+
 				}
+
+			} else {
+				w = element.getParent().getLeft();
+				if (w.isRed()) {
+					w.setColor(Color.BLACK);
+					element.getParent().setColor(Color.RED);
+					rightRotate(element.getParent());
+					w = element.getParent().getLeft();
+				}
+
+				if (w.getRight().isBlack() && w.getLeft().isBlack()) {
+					w.setColor(Color.RED);
+					element = element.getParent();
+				}
+
 				else {
-					
+					if (w.getLeft().isBlack()) {
+						w.getRight().setColor(Color.BLACK);
+						w.setColor(Color.RED);
+						leftRotate(w);
+						w = element.getParent().getLeft();
+					}
+					w.setColor(element.getParent().getColor());
+					element.getParent().setColor(Color.BLACK);
+					w.getLeft().setColor(Color.BLACK);
+					rightRotate(element.getParent());
+					element = root;
+
 				}
+
 			}
-			}
-			
+
 		}
-		
+
 		element.setColor(Color.BLACK);
 	}
 
