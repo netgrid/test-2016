fbf32a8646df36e41e85597b41c3682c30d8b44a
Michele Grigoli <grigolimichele@gmail.com>
2016-04-04 10:04:41 +0200

    implementato insert

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -1,5 +1,7 @@
 package it.netgrid.rbtrees;
 
+import it.netgrid.rbtrees.RbTreeElement.Color;
+
 public class RbTree<T extends Comparable<String>> {
 
 	private RbTreeElement<T> root;
@@ -17,15 +19,39 @@ public class RbTree<T extends Comparable<String>> {
 	private void deleteFixup(RbTreeElement<T> element) {
 	}
 
-	public void insert(T element) {
+	public void insert(RbTreeElement<T> element) {
+		RbTreeElement<T> y = null;
+		RbTreeElement<T> x = root;
+		
+		while (!x.equals(null)){
+			y = x;
+			
+			if ((element.getElement().toString().compareTo(x.getElement().toString())) < 0){
+				x = x.getLeft();
+			}
+			else{
+				x = x.getRight();
+			}	
+		}
+		//element.getParent() = y;
+		if (y.equals(null)){
+			root = element;
+		}
+		else if ((element.getElement().toString().compareTo(y.getElement().toString())) < 0){
+			y.setLeft(element);
+		}
+		else{
+			y.setRight(element);
+		}
+		
+		element.setLeft(null);
+		element.setRight(null);	
+		element.setColor(Color.RED);
+		//--RB-INSERT-FIXUP
 	}
 
 	public void delete(T element) {
-		/*
-		if(element.getLeft().equals()){
-			
-		}
-		*/
+
 	}
 
 	public T get(String element) {
