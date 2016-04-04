eb7dcb0e1b7240b68742b2ef6b74a18737d98274
Michele Grigoli <grigolimichele@gmail.com>
2016-04-04 10:11:22 +0200

    modifiche

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -1,5 +1,7 @@
 package it.netgrid.rbtrees;
 
+import it.netgrid.rbtrees.RbTreeElement.Color;
+
 public class RbTree<T extends Comparable<String>> {
 
 	private RbTreeElement<T> root;
@@ -35,7 +37,35 @@ public class RbTree<T extends Comparable<String>> {
 		
 	}
 
-	public void insert(T element) {
+	public void insert(RbTreeElement<T> element) {
+		RbTreeElement<T> y = null;
+		RbTreeElement<T> x = root;
+		
+		while (!x.equals(null)){
+			y = x;
+			
+			if ((element.getElement().toString().compareTo(x.getElement().toString())) < 0){
+				x = x.getLeft();
+			}
+			else{
+				x = x.getRight();
+			}	
+		}
+		//element.getParent() = y;
+		if (y.equals(null)){
+			root = element;
+		}
+		else if ((element.getElement().toString().compareTo(y.getElement().toString())) < 0){
+			y.setLeft(element);
+		}
+		else{
+			y.setRight(element);
+		}
+		
+		element.setLeft(null);
+		element.setRight(null);	
+		element.setColor(Color.RED);
+		//--RB-INSERT-FIXUP
 	}
 
 	public RbTreeElement<T> treeSuccessor(RbTreeElement<T> element){
@@ -52,20 +82,6 @@ public class RbTree<T extends Comparable<String>> {
 	}
 	
 	public void delete(RbTreeElement<T> element) {
-		RbTreeElement<T> result;
-		if((element.getLeft().equals(null))&&(element.getRight().equals(null))){
-			result= element;	
-		}
-		else{
-			result=treeSuccessor(element);
-		}
-		if(!(result.getLeft().equals(null))){
-			element=result.getLeft();
-		}
-		else{
-			element=result.getRight();
-		}
-		element.setParent(result.getParent());
 	}
 
 	public T get(String element) {
