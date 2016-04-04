0c7e53140874224f066e5234632b34bde65e0308
Alberto Harka <alberto_harka@yahoo.com>
2016-04-04 13:31:39 +0200

    modifica metodo Delete

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -102,6 +102,7 @@ public class RbTree<T extends Comparable<String>> {
 			deleteFixup(elementX);
 		}
 		
+		//18-----------------------------------------------------------------------------------------------|
 		return result;
 	}
 
