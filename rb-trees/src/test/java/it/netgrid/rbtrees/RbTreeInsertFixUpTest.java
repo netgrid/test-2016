caba257b5972a0a8c7597a19d777a27156786913
Giacomo Bonadiman <bonadimangiacomo@gmail.com>
2016-04-05 13:00:44 +0300

    Altro

--- rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
@@ -8,28 +8,30 @@ import static org.hamcrest.Matchers.notNullValue;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.codearte.jfairy.Fairy;
 import it.netgrid.rbtrees.RbTreeElement.Color;
 
 public class RbTreeInsertFixUpTest {
 	private RbTree<String> classUnderTest;
-	//private Fairy fairy;
+	private Fairy fairy;
 	
 	@Before
 	public void init() {
 		this.classUnderTest = new RbTree<String>();
+		this.fairy = Fairy.create();
 		
 	}
 	@Test
 	public void insertFixUpEmpty() {
 		RbTree<String> output = RbTreeGenerator.empty();
-		this.classUnderTest.insertFixup(output.getRoot());
+		this.classUnderTest.insertFixup(classUnderTest.getRoot());
 		assertThat("null root", output.getRoot(), equalTo(null));
 	}
 	
 	@Test
 	public void insertFixUpOne() {
 		RbTree<String> output = RbTreeGenerator.one();
-		this.classUnderTest.insertFixup(output.getRoot());
+		this.classUnderTest.insertFixup(classUnderTest.getRoot());
 		assertThat("root is black", output.getRoot().getColor(), equalTo(Color.BLACK));
 		assertThat("null left", output.getRoot().getLeft(), equalTo(null));
 		assertThat("null right", output.getRoot().getRight(), equalTo(null));
@@ -39,7 +41,7 @@ public class RbTreeInsertFixUpTest {
 	@Test
 	public void insertFixUpSix() {
 		RbTree<String> output = RbTreeGenerator.six();
-		this.classUnderTest.insertFixup(output.getRoot());
+		this.classUnderTest.insertFixup(classUnderTest.getRoot());
 		assertThat("root is black", output.getRoot().getColor(), equalTo(Color.BLACK));
 		assertThat("not null left", output.getRoot().getLeft(), notNullValue());
 		assertThat("not null right", output.getRoot().getRight(), notNullValue());
