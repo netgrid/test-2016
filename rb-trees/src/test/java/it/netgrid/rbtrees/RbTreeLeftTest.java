c7bcfafe27afe2bf17b9c39a261cceea1527292b
Roberto Indelicato <indelicatoroberto@hotmail.com>
2016-04-05 12:38:21 +0200

    no message

--- rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeLeftTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeLeftTest.java
@@ -4,16 +4,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import io.codearte.jfairy.Fairy;
+import it.netgrid.rbtrees.RbTreeElement.Color;
 
 public class RbTreeLeftTest {
-	RbTree<BasicRbTreeDecorator<Object>> classUnderTest;
-	
+	RbTree<String> classUnderTest;
+
 	private Fairy fairy;
 	
 	@Before
 	public void init(){
-		this.classUnderTest =new RbTree<BasicRbTreeDecorator<Object>>();
+		this.classUnderTest = new RbTree<String>();
+		this.fairy = Fairy.create();
+	}
+	
+	
+	@Test
+	public void testLeftRotate() {
+			classUnderTest =RbTreeGenerator.one();
+			this.classUnderTest.leftRotate(classUnderTest.getRoot());
+			assertThat("root is black", classUnderTest.getRoot().getColor(), equalTo(Color.BLACK));
+			assertThat("null left", classUnderTest.getRoot().getLeft(), equalTo(null));
+			assertThat("null right", classUnderTest.getRoot().getRight(), equalTo(null));
 	}
 }
