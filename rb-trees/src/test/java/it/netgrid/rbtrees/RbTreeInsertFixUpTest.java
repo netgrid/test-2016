ec019f1901dd67b8adbc2c28ed9f101216669db3
Giacomo Bonadiman <bonadimangiacomo@gmail.com>
2016-04-05 09:58:00 +0300

    piccole modifiche

--- rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
@@ -2,11 +2,14 @@ package it.netgrid.rbtrees;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import io.codearte.jfairy.Fairy;
+import it.netgrid.rbtrees.RbTreeElement.Color;
 
 public class RbTreeInsertFixUpTest {
 	private RbTree<BasicRbTreeDecorator<Object>> classUnderTest;
@@ -18,25 +21,26 @@ public class RbTreeInsertFixUpTest {
 		this.fairy = Fairy.create();
 	}
 	@Test
-	public void testNullCompareToNull() {
-		int result = this.classUnderTest.compareTo(null);
-		assertThat("null equals null", result, equalTo(0));
+	public void testEmpty() {
+		RbTree<String> output = RbTreeGenerator.empty();
+		assertThat("null root", output.getRoot(), equalTo(null));
 	}
 	
 	@Test
-	public void testNullCompareToEmptyString() {
-		int result = this.classUnderTest.compareTo("");
-		assertThat("null smaller than empty", result, equalTo(-1));
-	}
-	
-	@Test
-	public void testInertNotEmptyCompareTo() {
-		String item = this.fairy.textProducer().latinSentence();
-		String compare = this.fairy.textProducer().latinSentence();
-		this.classUnderTest = new RbTree<BasicRbTreeDecorator<Object>>();
-		int result = this.classUnderTest.compareTo(compare);
-		int check = item.compareTo(compare);
+	public void testOne() {
+		RbTree<String> output = RbTreeGenerator.one();
+		assertThat("root is black", output.getRoot().getColor(), equalTo(Color.BLACK));
+		assertThat("null left", output.getRoot().getLeft(), equalTo(null));
+		assertThat("null right", output.getRoot().getRight(), equalTo(null));
 		
-		assertThat("same results", result, equalTo(check));
+	}
+	
+	@Test
+	public void testSix() {
+		RbTree<String> output = RbTreeGenerator.six();
+		assertThat("root is black", output.getRoot().getColor(), equalTo(Color.BLACK));
+		assertThat("not null left", output.getRoot().getLeft(), notNullValue());
+		assertThat("not null right", output.getRoot().getRight(), notNullValue());
+		assertThat("not equal", output.getRoot().getLeft(), not(equals(output.getRoot().getRight())));	
 	}
 }
