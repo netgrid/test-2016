7dfd87bb3cad836240610530a653a4061f6916bc
Davide Bolla <gqc23566@figjs.com>
2016-04-06 12:58:10 +0200

    update

--- rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeDeleteFixupTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeDeleteFixupTest.java
@@ -24,6 +24,7 @@ public class RbTreeDeleteFixupTest {
 		this.classunderTest.deleteFixup(classunderTest.getRoot());
 		//assertThat("null equals null", result, equalTo(0));
 		assertThat("null root", classunderTest.getRoot(), equalTo(null));
+		RbTreeGenerator.writeDotFile(classunderTest);
 	}
 	
 	@Test
@@ -33,29 +34,30 @@ public class RbTreeDeleteFixupTest {
 		assertThat("root is black", classunderTest.getRoot().getColor(), equalTo(Color.BLACK));
 		assertThat("null left", classunderTest.getRoot().getLeft(), equalTo(null));
 		assertThat("null right", classunderTest.getRoot().getRight(), equalTo(null));
-		
+		RbTreeGenerator.writeDotFile(classunderTest);
 	}
 
 	@Test
 	public void testSix() {
 		classunderTest = RbTreeGenerator.six();
-		this.classunderTest.deleteFixup(classunderTest.getRoot());
+		this.classunderTest.deleteFixup(classunderTest.getRoot().getLeft().getLeft());
 		assertThat("root is black", classunderTest.getRoot().getColor(), equalTo(Color.BLACK));
 		assertThat("not null left", classunderTest.getRoot().getLeft(), notNullValue());
 		assertThat("not null right", classunderTest.getRoot().getRight(), notNullValue());
 		assertThat("not equal", classunderTest.getRoot().getLeft(), not(equals(classunderTest.getRoot().getRight())));	
+		RbTreeGenerator.writeDotFile(classunderTest);
 	}
 	
 	@Test
 	public void testSpec() {
 		classunderTest = RbTreeGenerator.six();
 		//RbTreeElement<String> left= parent.getLeft();
-		//this.classunderTest.deleteFixup(classunderTest.getRoot());
+		//this.classunderTest.deleteFixup(classunderTest.getRoot().getLeft().getRight());
 		this.classunderTest.deleteFixup(classunderTest.getRoot().getLeft().getLeft());
 		assertThat("root is black", classunderTest.getRoot().getColor(), equalTo(Color.BLACK));
 		assertThat("null left", classunderTest.getRoot().getLeft(), equalTo(null));
 		assertThat("null right", classunderTest.getRoot().getRight(), equalTo(null));
-		
+		RbTreeGenerator.writeDotFile(classunderTest);
 	}
 	
 }
