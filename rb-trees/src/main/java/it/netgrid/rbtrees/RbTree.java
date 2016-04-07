a48aed8e4584aeb7927cb66c85cc075552a5ecf6
Matteo Bissoli <matteo.bissoli@netgrid.it>
2016-04-07 12:49:29 +0200

    Sistemati gli import

--- rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
+++ rb-trees/src/main/java/it/netgrid/rbtrees/RbTree.java
@@ -1,90 +1,77 @@
 package it.netgrid.rbtrees;
 
-
-import java.awt.Color;
-import java.util.List;
-
+import it.netgrid.rbtrees.RbTreeElement.Color;
 
 public class RbTree<T extends Comparable<String>> {
 
 	private RbTreeElement<T> root;
 
-	
-	//procedura che restituisce  un puntatore all'elemeneto minimo del sottoalbero con radice in un nodo x
-	private RbTreeElement<T> treeMinimum(RbTreeElement<T> element){
-		RbTreeElement<T> result=null;
-		while(!(element.getLeft().equals(null))){
-			result=element.getLeft();
-		}
-		return result;
-	}
-	
-	//procedura che restituisce  un puntatore all'elemeneto massimo del sottoalbero con radice in un nodo x
-	private RbTreeElement<T> treeMaximum(RbTreeElement<T> element){
-		RbTreeElement<T> result=null;
-		while(!(element.getRight().equals(null))){
-			result=element.getRight();
+	// procedura che restituisce un puntatore all'elemeneto minimo del
+	// sottoalbero con radice in un nodo x
+	private RbTreeElement<T> treeMinimum(RbTreeElement<T> element) {
+		RbTreeElement<T> result = null;
+		while (!(element.getLeft().equals(null))) {
+			result = element.getLeft();
 		}
 		return result;
 	}
 
-
+	// procedura che restituisce un puntatore all'elemeneto massimo del
+	// sottoalbero con radice in un nodo x
+	private RbTreeElement<T> treeMaximum(RbTreeElement<T> element) {
+		RbTreeElement<T> result = null;
+		while (!(element.getRight().equals(null))) {
+			result = element.getRight();
+		}
+		return result;
+	}
 
 	public void leftRotate(RbTreeElement<T> element) {
-		RbTreeElement<T> y = element.getRight();						//Imposto y
-		element.setRight(y.getLeft());								//Sposto il sottoalbero sinistro di x nel sottoalbero 
-		if(y.getLeft() != null){									//sinistro destro di x
+		RbTreeElement<T> y = element.getRight(); // Imposto y
+		element.setRight(y.getLeft()); // Sposto il sottoalbero sinistro di x
+										// nel sottoalbero
+		if (y.getLeft() != null) { // sinistro destro di x
 			y.getLeft().setParent(element);
 		}
-		y.setParent(element.getParent());							// collego il padre di x con quello di y
-		if(element.getParent() == null){
+		y.setParent(element.getParent()); // collego il padre di x con quello di
+											// y
+		if (element.getParent() == null) {
 			root = y;
-		}
-		else{
-			if(element == element.getParent().getLeft()){
+		} else {
+			if (element == element.getParent().getLeft()) {
 				element.getParent().setLeft(y);
-			}
-			else{
+			} else {
 				element.getParent().setRight(y);
 			}
 		}
-		y.setLeft(element);										//Pone x a sinistra di y 
-		element.setParent(y);
-		}
-
-		
-
-	
-
-	public void rightRotate(RbTreeElement<T> element) {
-		RbTreeElement<T> y = element.getLeft();						//Imposto y
-		element.setLeft(y.getRight());								//Sposto il sottoalbero destro di x nel sottoalbero 
-		if(y.getRight() != null){									//sinistro sinistro di x
-			y.getRight().setParent(element);
-		}
-		y.setParent(element.getParent());							// collego il padre di x con quello di y
-		if(element.getParent() == null){
-			root = y;
-		}
-		else{
-			if(element == element.getParent().getRight()){
-				element.getParent().setRight(y);
-			}
-			else{
-				element.getParent().setLeft(y);
-			}
-		}
-		y.setRight(element);										//Pone x a destra di y 
+		y.setLeft(element); // Pone x a sinistra di y
 		element.setParent(y);
 	}
 
-
-
-	
+	public void rightRotate(RbTreeElement<T> element) {
+		RbTreeElement<T> y = element.getLeft(); // Imposto y
+		element.setLeft(y.getRight()); // Sposto il sottoalbero destro di x nel
+										// sottoalbero
+		if (y.getRight() != null) { // sinistro sinistro di x
+			y.getRight().setParent(element);
+		}
+		y.setParent(element.getParent()); // collego il padre di x con quello di
+											// y
+		if (element.getParent() == null) {
+			root = y;
+		} else {
+			if (element == element.getParent().getRight()) {
+				element.getParent().setRight(y);
+			} else {
+				element.getParent().setLeft(y);
+			}
+		}
+		y.setRight(element); // Pone x a destra di y
+		element.setParent(y);
+	}
 
 	private void insertFixup(RbTreeElement<T> element) {
 
-		
 	}
 
 	private void deleteFixup(RbTreeElement<T> element) {
@@ -93,67 +80,63 @@ public class RbTree<T extends Comparable<String>> {
 	public void insert(T element) {
 	}
 
-	public RbTreeElement<T> treeSuccessor(RbTreeElement<T> element){
+	public RbTreeElement<T> treeSuccessor(RbTreeElement<T> element) {
 		RbTreeElement<T> result;
-		if(element.getRight().equals(null)){
+		if (element.getRight().equals(null)) {
 			return treeMinimum(element.getRight());
 		}
-		result=element.getParent();
-		while((!(result.equals(null)))&&(element.equals(result.getRight()))){
-			element=result;
-			result=result.getParent();
+		result = element.getParent();
+		while ((!(result.equals(null))) && (element.equals(result.getRight()))) {
+			element = result;
+			result = result.getParent();
 		}
 		return result;
 	}
-	
-	//funzione che elemina un nodo dall'albero
+
+	// funzione che elemina un nodo dall'albero
 	public RbTreeElement<T> delete(RbTreeElement<T> elementZ) {
 		RbTreeElement<T> result;
 		RbTreeElement<T> elementX;
-		
-		//1-6----------------------------------------------------------------------------------------------|
-		if((elementZ.getLeft().equals(null))&&(elementZ.getRight().equals(null))){
-			result= elementZ;	
+
+		// 1-6----------------------------------------------------------------------------------------------|
+		if ((elementZ.getLeft().equals(null)) && (elementZ.getRight().equals(null))) {
+			result = elementZ;
+		} else {
+			result = treeSuccessor(elementZ);
 		}
-		else{
-			result=treeSuccessor(elementZ);
+		if (!(result.getLeft().equals(null))) {
+			elementX = result.getLeft();
+		} else {
+			elementX = result.getRight();
 		}
-		if(!(result.getLeft().equals(null))){
-			elementX=result.getLeft();
-		}
-		else{
-			elementX=result.getRight();
-		}
-		
-		//7------------------------------------------------------------------------------------------------|
+
+		// 7------------------------------------------------------------------------------------------------|
 		elementX.setParent(result.getParent());
-		
-		//8-12---------------------------------------------------------------------------------------------|
-		if(result.getParent().equals(null)){
+
+		// 8-12---------------------------------------------------------------------------------------------|
+		if (result.getParent().equals(null)) {
 			this.setRoot(elementX);
-		}
-		else if (result.equals(result.getParent().getLeft())) {
+		} else if (result.equals(result.getParent().getLeft())) {
 			result.getParent().setLeft(elementX);
-		}
-		else {
+		} else {
 			result.getParent().setRight(elementX);
 		}
-		
-		//13-15--------------------------------------------------------------------------------------------|
-		if(!(result.equals(elementZ))){
-			//copia dati satellite
+
+		// 13-15--------------------------------------------------------------------------------------------|
+		if (!(result.equals(elementZ))) {
+			// copia dati satellite
 			elementZ.setColor(result.getColor());
 			elementZ.setLeft(result.getRight());
 			elementZ.setRight(result.getRight());
 			elementZ.setParent(result.getParent());
 		}
-		
-		//16-17--------------------------------------------------------------------------------------------|
-		if(result.getColor().equals(Color.BLACK)){
+
+		// 16-17--------------------------------------------------------------------------------------------|
+		if (result.getColor().equals(Color.BLACK)) {
 			deleteFixup(elementX);
 		}
-		
-		//18-----------------------------------------------------------------------------------------------|
+
+		// 18-----------------------------------------------------------------------------------------------|
 		return result;
 	}
 
