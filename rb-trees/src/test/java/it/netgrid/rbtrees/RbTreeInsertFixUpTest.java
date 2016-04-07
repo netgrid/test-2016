bf378664389918bcfd46a13b2d3732dec233a7a4
Giacomo Bonadiman <bonadimangiacomo@gmail.com>
2016-04-07 12:56:30 +0300

    Manca il 2, verificare il 3.

--- rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
+++ rb-trees/src/test/java/it/netgrid/rbtrees/RbTreeInsertFixUpTest.java
@@ -2,7 +2,9 @@ package it.netgrid.rbtrees;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
+
+import static org.hamcrest.Matchers.notNullValue;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,10 +12,8 @@ import io.codearte.jfairy.Fairy;
 import it.netgrid.rbtrees.RbTreeElement.Color;
 
 /**
- * iTest.getRoot() -> Radice
- * element -> Nodo
- * base -> Per il calcolo di posizioni di ogni nodo
- * child -> Figlio di element
+ * iTest.getRoot() -> Radice element -> Nodo base -> Per il calcolo di posizioni
+ * di ogni nodo child -> Figlio di element
  */
 public class RbTreeInsertFixUpTest {
 	// private RbTree<String> classUnderTest;
@@ -71,49 +71,93 @@ public class RbTreeInsertFixUpTest {
 		RbTreeGenerator.writeDotFile(iTest);
 		assertThat("root is black", iTest.getRoot().getColor(), equalTo(Color.BLACK));
 	}
-	
+
 	@Test
-	public void assertThatFixUp(){
+	public void assertThatFixUp() {
 		RbTree<String> iTest = RbTreeGenerator.six();
 		RbTreeElement<String> element = iTest.getRoot().getRight();
 		iTest.insertFixup(element);
-		
-		
+
 		// Radice Nera
-		if(iTest.getRoot()!= null){
-		assertThat("root is black", iTest.getRoot().getColor(), equalTo(Color.BLACK));
+		if (iTest.getRoot() != null) {
+			assertThat("root is black", iTest.getRoot().getColor(), equalTo(Color.BLACK));
 		}
-		
+
 		// Ricorda che Nodo Null == Nero (nessuna istruzione)
 
-		String base = iTest.getRoot().getElement();
-		BasicRbTreeDecorator<String> child = new BasicRbTreeDecorator<String>(base + base);
-		child.setColor(Color.RED);
-		child.setParent(element);
-		element.setRight(child);
-		
 		// Nodo Rosso e Figli Neri
 		if (element.isRed()) {
-			child.setColor(Color.BLACK);
-			assertThat("node is red", element.getColor(), equalTo(Color.RED));
-			assertThat("children are black", child.getColor(), equalTo(Color.BLACK));
+			if (element.getLeft().isBlack() && element.getRight().isBlack()) {
+				assertThat("node is red", element.getColor(), equalTo(Color.RED));
+				assertThat("child left is black", element.getLeft().getColor(), equalTo(Color.BLACK));
+				assertThat("child right is black", element.getRight().getColor(), equalTo(Color.BLACK));
+			} else {
+				assertThat("node is red", element.getColor(), equalTo(Color.RED));
+				assertThat("child is red or null", element.getLeft().getColor(), notNullValue());
+				assertThat("child is red or null", element.getRight().getColor(), notNullValue());
+				return;
+			}
 		}
-		
-		// Ogni Radice ha lo stesso numero di Nodi Neri
-       if(element.isBlack()){
-		if (iTest.getRoot().getLeft().getColor().toString()
-				.compareTo(iTest.getRoot().getRight().getColor().toString()) == 0) {
-          
-			assertThat("same number of black nodes in root", iTest.getRoot().getLeft().getColor(),
-					equalTo(iTest.getRoot().getRight().getColor()));
+	}
+
+	// Ogni Radice ha lo stesso numero di Nodi Neri
+
+	int counter;
+	int counterL;
+	int counterR;
+
+	public int visita(RbTreeElement<String> element) {
+		RbTree<String> iTest = RbTreeGenerator.six();
+		element = iTest.getRoot().getRight();
+		iTest.insertFixup(element);
+
+		if (element == null) {
+			counter = 1;
+			return counter;
+		} else {
+			counter = 0;
+			visitaLeft(element);
+			return counter;
+		}
+
+	}
+
+	public int visitaLeft(RbTreeElement<String> element) {
+
+		if (element == null) {
+			counterL = 1;
+			return counterL;
 		} else {
 
-			assertThat("not equal number", iTest.getRoot().getLeft(), not(equals(iTest.getRoot().getRight())));
+			element.getLeft();
+			counterL = 0;
+			visitaRight(element);
+			return counterL;
 		}
-        }
 	}
+
+	public int visitaRight(RbTreeElement<String> element) {
+		int counterR;
+		if (element == null) {
+			counterR = 1;
+			return counterR;
+		} else {
+
+			element.getRight();
+			counterR = 0;
+			visitaCheck(element);
+			return counterR;
+
+		}
+
 	}
-	
 
+	public int visitaCheck(RbTreeElement<String> element) {
+		int fCounter;
 
+		fCounter = counter + counterL + counterR;
+		assertThat("same number of black nodes in root", visitaLeft(element), equalTo(visitaRight(element)));
+		return visita(element) + visitaLeft(element) + visitaRight(element) + fCounter;
 
+	}
+}
